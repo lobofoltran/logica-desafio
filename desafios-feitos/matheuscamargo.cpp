1bb1f37bebbc13d1c3d75c918ca307121577e690
Gustavo Lobo <89169668+lobofoltran@users.noreply.github.com>
2021-08-30 19:29:33 -0300

    Update matheuscamargo.cpp

--- desafios-feitos/matheuscamargo.cpp
+++ desafios-feitos/matheuscamargo.cpp
@@ -1,21 +1,27 @@
 #include<iostream>
+using namespace std;
 
 int main () {
-	char nome[50];
+	string nome2;
 	int pretensao = 0;
 	
-	std::cout	<< "Insira o seu nome: ";
-	std::cin	>> nome;
+	cout	<< "Insira o seu nome: ";
+	getline(cin, nome2); // Usado para pegar o nome completo.
 	
-	std::cout	<< "Insira a sua pretensao salarial: ";
-	std::cin	>> pretensao;
+	cout	<< "Insira a sua pretensao salarial: ";
+	cin	>> pretensao;
 	
-	if (pretensao <= 1299) {
-		std::cout	<< "O candidato se encaixa em um cargo de auxiliar de producao.";
-	} else if (pretensao <= 2499) {
-		std::cout	<< "O candidato se encaixa em um cargo de lideranca.";
+	if (pretensao > 0 && pretensao < 1300) {
+		cout	<< endl << "Nome do candidato: " << nome2 << endl;
+		cout	<< "O candidato se encaixa em um cargo de auxiliar de producao.";
+	} else if (pretensao >= 1300 && pretensao < 2500) {		
+		cout	<< endl << "Nome do candidato: " << nome2 << endl;
+		cout	<< "O candidato se encaixa em um cargo de lideranca.";
 	} else if (pretensao >= 2500) {
-		std::cout	<< "O candidato nao se encaixa no que a empresa procura.";
+		cout	<< endl << "Nome do candidato: " << nome2 << endl;
+		cout	<< "O candidato nao se encaixa no que a empresa procura.";
+	} else {
+		cout	<< endl << "Digite um dado valido!";
 	}
 	
 	return 0;
