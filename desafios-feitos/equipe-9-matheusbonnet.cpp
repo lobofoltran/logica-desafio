bb974476175eb38e9c80348578bba0ca441ee240
Gustavo Lobo <gustavoqe.75@gmail.com>
2021-09-22 02:59:02 -0300

    Att 22/09

--- desafios-feitos/equipe-9-matheusbonnet.cpp
+++ desafios-feitos/equipe-9-matheusbonnet.cpp
@@ -5,38 +5,40 @@ salários maiores que 1900 resultarão em 2% de imposto, 2500 3%,
 baixo de 1900 não precisam pagar. */
 
 #include <iostream>
+#include <clocale>
 
 int main () {
 	float salario = 0;
 	float salarioForm = 0; 	// Salário Fórmula.
 	float salarioForm2 = 0; // Salário Fórmula 2.
+	setlocale(LC_ALL,"Portuguese");
 	
-	std::cout	<< "Digite o valor do salario: ";
+	std::cout	<< "--- * Calculadora de Imposto * ----" << std::endl << std::endl << "Digite o valor do salário (R$): ";
 	std::cin	>> salario;
 	
 	if (salario < 1900) {
-		std::cout	<< std::endl << "Imposto cobrado (0% de imposto): " << "0" << std::endl; // Neste exemplo a Fórmula de Salário será sempre 0.
-		std::cout	<< "Salario (sem imposto): " << salario; 				 // Quando o salário for menor que 1900, não haverá cobrança de impostos.
+		std::cout	<< std::endl << "Imposto cobrado (0% de imposto): R$ " << "0" << std::endl; // Neste exemplo a Fórmula de Salário será sempre 0.
+		std::cout	<< "Salário (sem imposto): R$ " << salario;				 // Quando o salário for menor que 1900, não haverá cobrança de impostos.
 	} else if (salario >= 1900 && salario < 2500) {
 		salarioForm = ( salario * 2 ) / 100; 							// Formúla = Cálculo de Imposto. (Salário * 2) / 100 (2 seria de 2% seria o valor do imposto). 
 		salarioForm2 = salario - salarioForm; 							// A seguir calculamos o valor do Salário menos o imposto.
-		std::cout	<< std::endl << "Imposto cobrado (2% de imposto): " << salarioForm; 	// Resultado da Fórmula de porcentagem.
-		std::cout	<< << std::endl << "Salario (-2% de imposto): " << salarioForm2; 	// Resultado da Fórmula de Salário menos Porcentagem de imposto.
+		std::cout	<< std::endl << "Imposto cobrado (2% de imposto): R$ " << salarioForm; 	// Resultado da Fórmula de porcentagem.
+		std::cout	<< std::endl << "Salário (-2% de imposto): R$ " << salarioForm2; 	// Resultado da Fórmula de Salário menos Porcentagem de imposto.
 	} else if (salario >= 2500 && salario < 3700) {
 		salarioForm = ( salario * 3 ) / 100;
 		salarioForm2 = salario - salarioForm;
-		std::cout	<< std::endl << "Imposto cobrado (3% de imposto): " << salarioForm << std::endl;
-		std::cout	<< "Salario (-3% de imposto): " << salarioForm2;
+		std::cout	<< std::endl << "Imposto cobrado (3% de imposto): R$ " << salarioForm << std::endl;
+		std::cout	<< "Salário (-3% de imposto): R$ " << salarioForm2;
 	} else if (salario >= 3700 && salario < 5000) {
 		salarioForm = ( salario * 4.5 ) / 100;
 		salarioForm2 = salario - salarioForm;
-		std::cout	<< std::endl << "Imposto cobrado (4.5% de imposto): " << salarioForm << std::endl;
-		std::cout	<< "Salario (-4.5% de imposto): " << salarioForm2;
+		std::cout	<< std::endl << "Imposto cobrado (4.5% de imposto): R$ " << salarioForm << std::endl;
+		std::cout	<< "Salário (-4.5% de imposto): R$ " << salarioForm2;
 	} else if (salario >= 5000) {
 		salarioForm = ( salario * 6 ) / 100;
 		salarioForm2 = salario - salarioForm;
-		std::cout	<< std::endl << "Imposto cobrado (6% de imposto): " << salarioForm << std::endl;
-		std::cout	<< "Salario (-6% de imposto): " << salarioForm2;
+		std::cout	<< std::endl << "Imposto cobrado (6% de imposto): R$ " << salarioForm << std::endl;
+		std::cout	<< "Salário (-6% de imposto): R$ " << salarioForm2;
 	}
 	
 	return 0;
